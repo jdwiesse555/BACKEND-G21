6da8ddb207d44698eec22353f36daf2d6e49aee4
javier wiesse <javier.wiesse@gmail.com>
2024-11-14 21:06:42 -0500

    v03

---
+++ dia_1/1_variables.py
@@ -0,0 +1,14 @@
+# tipo de datos
+nombre = "javier 't "
+apellido = 'wiesse "sd"'
+enunciado = '''hola 
+como estas'''
+# o
+enunciado2 = """hola 
+espero verte pronto
+como estas"""
+edad = 58
+casado = True
+estatura = 1.82
+print("hola  " + nombre +" "+apellido+enunciado2)
+
