f6682dbc0e021f1159c9bc9237a16c0278977d54
javier wiesse <javier.wiesse@gmail.com>
2024-11-14 23:00:59 -0500

    da1v3

--- dia_1/3_usodevariables.py
+++ dia_1/3_usodevariables.py
@@ -26,3 +26,17 @@ texto='el dia de hoy fue a clase y saque 20'
 print(texto[4:10])
 print(texto[:10])
 print(texto[7:])
+
+
+#numeros
+
+resultado = 3 + 10.75
+print(resultado)
+
+#forma para hacer mas legible un numero grande
+numerazo= 1_010_010_222
+print(numerazo+1)
+
+numero4 , numero5 = 3 , 56
+print(numero5)
+print(numero4)
