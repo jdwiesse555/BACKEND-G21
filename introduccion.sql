6a85f3e0411a0296287544cc81b41885781d1216
javier wiesse <javier.wiesse@gmail.com>
2024-11-20 21:15:42 -0500

    semana02

---
+++ introduccion.sql
@@ -0,0 +1,8 @@
+-- asi se define los comentarios 
+-- DDL  ES UN SUBLENGUAJE DE SQL que sirve para definir como almacena los datos
+CREATE DATABASE pruebas;
+-- nos conecta a la base datos
+\connect pruebas
+
+CREATE TABLE 
+
