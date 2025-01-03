b58f683a2ef961ef0a80b7d5f22b54c7d3f9d703
javier wiesse <javier.wiesse@gmail.com>
2025-01-02 22:17:07 -0500

    inicio

--- Lista_escolares/gestion/admin.py
+++ Lista_escolares/gestion/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Producto
 
+class ProductoAdmin(admin.ModelAdmin):
+    # loa campos que voy a mostrar de mi table producto
+    list_display = ['nombre']
+    # los campos que voy a trabajar
+    fields = ['nombre','descripcion']
 # Register your models here.
+#aca indico que modelo de esta aplicacopn puedo gestionar en el panel adminstrativo
+admin.site.register(Producto,ProductoAdmin)
